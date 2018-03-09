51cf10cd79ff5e91a79d9d1f5ed94d0beccd2e6f
Matthew Bowles <mbowles@za-engineering.com>
2018-03-09 12:52:07 -0700

    Factored some LEDE-specific stuff out of models.py into LEDE_config.py

--- src/models.py
+++ src/models.py
@@ -8,75 +8,9 @@ from graphos.sources.model import ModelDataSource
 from graphos.sources.model import SimpleDataSource
 from graphos.renderers import gchart
 
-SENSOR_ELEVATION = 9499
-DEPTH_MIN_VALUE = 0
-DEPTH_MAX_VALUE = 100
-
-OUTFLOW_MIN_VALUE = 0
-OUTFLOW_MAX_VALUE = 1
-
-FILLRATE_MIN_VALUE = -2
-FILLRATE_MAX_VALUE = 2
+from LEDE_config import *
 
 
-STORAGE_VOLUME = {  
-#   ELEV : ACRE-FEET
-    9494 : 0.0,
-    9495 : 0.0,
-    9496 : 0.0,
-    9497 : 3.6,
-    9498 : 7.1,
-    9499 : 13.7,
-    9500 : 20.2,
-    9501 : 29.0,
-    9502 : 37.9,
-    9503 : 48.7,
-    9504 : 59.5,
-    9505 : 72.2,
-    9506 : 84.9,
-    9507 : 99.1,
-    9508 : 113.4,
-    9509 : 129.1,
-    9510 : 144.9,
-    9511 : 162.1,
-    9512 : 179.4,
-    9513 : 198.0,
-    9514 : 216.5,
-    9515 : 236.3,
-    9516 : 256.1,
-    9517 : 277.1,
-    9518 : 298.0,
-    9519 : 320.0,
-    9520 : 342.1,
-    9521 : 342.1,
-    9522 : 388.3,
-    9523 : 412.6,
-    9524 : 436.8,
-    9525 : 462.1,
-    9526 : 487.4,
-    9527 : 513.5,
-    9528 : 539.6,
-    9529 : 566.5,
-    9530 : 593.5,
-    9531 : 621.2,
-    9532 : 649.0,
-    9533 : 677.6,
-    9534 : 706.2,
-    9535 : 735.7,
-    9536 : 765.1,
-    9537 : 795.1,
-    9538 : 825.9,
-    9539 : 857.3,
-    9540 : 888.6,
-    9541 : 921.0,
-    9542 : 953.3,
-    9543 : 986.6,
-    9544 : 1020.0,
-    9545 : 1053.3,
-    9546 : 1086.7,
-    9547 : 1120.0,
-    9548 : 1153.4,
-}
 
 class Measurement(models.Model):
     stamp = models.DateTimeField()
